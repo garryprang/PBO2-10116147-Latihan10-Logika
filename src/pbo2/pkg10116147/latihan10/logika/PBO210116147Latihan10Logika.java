e0d741cbf8407d5393a33df135ab0c7d1e379192
grryv <grryv@grry>
2020-10-21 19:46:41 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan10/logika/PBO210116147Latihan10Logika.java
@@ -0,0 +1,20 @@
+package pbo2.pkg10116147.latihan10.logika;
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk mengimplementasikan logika.
+ */
+
+public class PBO210116147Latihan10Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+    
+}
